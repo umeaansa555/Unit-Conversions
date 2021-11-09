8717d931c939f1dbe90d4b5c800d6f877eede3c4
P4 <umeaansa555@ed.amdsb.ca>
2021-11-09 13:15:30 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Unit_Conversions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.choiceBox = new System.Windows.Forms.TextBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(46, 43);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(59, 52);
+            this.infoLabel.TabIndex = 0;
+            this.infoLabel.Text = "1. in to cm\r\n2. ft to cm\r\n3. yd to m\r\n4. yd to km";
+            // 
+            // choiceBox
+            // 
+            this.choiceBox.Location = new System.Drawing.Point(155, 133);
+            this.choiceBox.Name = "choiceBox";
+            this.choiceBox.Size = new System.Drawing.Size(100, 20);
+            this.choiceBox.TabIndex = 1;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(155, 159);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.choiceBox);
+            this.Controls.Add(this.infoLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.TextBox choiceBox;
+        private System.Windows.Forms.TextBox inputBox;
+    }
+}
+
